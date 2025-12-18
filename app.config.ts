9f9344b4608885dd9182dda4a7baf135f52388bc
glebungle <cjy031212@gmail.com>
2025-12-18 17:09:49 +0900

    config ios 수정

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
+// app.config.ts
 import 'dotenv/config';
 import { ConfigContext, ExpoConfig } from 'expo/config';
 
@@ -6,10 +7,10 @@ const kakaoNativeAppKey = process.env.EXPO_PUBLIC_NATIVE_APP_KEY;
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   owner: 'starsam',
-  name: 'mumuri', // 스토어에 표시될 이름
+  name: 'mumuri',
   slug: 'mumuri',
   version: '1.0.0',
-  orientation: 'portrait', // 세로 모드 고정
+  orientation: 'portrait',
   icon: './assets/images/logo.png',
   scheme: 'mumuri',
   userInterfaceStyle: 'automatic',
@@ -20,16 +21,15 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     backgroundColor: '#FFFCF5',
   },
   ios: {
-    supportsTablet: false, 
+    supportsTablet: true,
     bundleIdentifier: 'com.growdy.mumuri', 
-    googleServicesFile: "./GoogleService-Info.plist",
+    googleServicesFile: "./GoogleService-Info.plist" ,
     infoPlist: {
       CFBundleURLTypes: [
         {
-          CFBundleURLSchemes: [`kakao${kakaoNativeAppKey}`],
+          CFBundleURLSchemes: ['mumuri', `kakao${kakaoNativeAppKey}`],
         },
       ],
-      ITSAppUsesNonExemptEncryption: false,
       NSCameraUsageDescription: '미션 사진 촬영을 위해 카메라 접근이 필요합니다.',
       NSPhotoLibraryAddUsageDescription: '촬영한 사진을 앨범에 저장합니다.',
       NSLocationWhenInUseUsageDescription: '사진 촬영 시 촬영 장소를 기록하기 위해 위치 정보가 필요합니다.',
@@ -72,16 +72,14 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         android: {
           extraMavenRepos: ['https://devrepo.kakao.com/nexus/content/groups/public/'],
         },
-        ios: {
-          useFrameworks: 'static',
-        }
       },
     ],
     [
       'expo-notifications',
       {
         icon: './assets/images/logo.png', 
-        color: '#FFFCF5', 
+        color: '#FF9191', 
+        sounds: [], 
       },
     ],
     [
@@ -96,17 +94,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         },
       },
     ],
-    ['react-native-android-widget', {
-      widgets: [
-        {
-          name: 'CoupleDDayWidget',
-          label: '무무리 디데이!',
-          renderWidget: 'widgets/CoupleDDayWidget.tsx',
-          minWidth: '80dp',
-          minHeight: '80dp',
-        },
-      ],
-    }]
   ],
   experiments: {
     typedRoutes: true,
