0f5afca0bc24997e0fcc932db33072117f466c23
glebungle <cjy031212@gmail.com>
2026-01-16 22:33:08 +0900

    버전정보 변경

--- app.config.ts
+++ app.config.ts
@@ -24,7 +24,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     supportsTablet: false,
     bundleIdentifier: 'com.growdy.mumuri', 
     googleServicesFile: "./GoogleService-Info.plist" ,
-    buildNumber: '4', 
+    buildNumber: '5', 
     infoPlist: {
       CFBundleURLTypes: [
         {
